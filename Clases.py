01fa0d09d62ccc0680f34ef59777ea9c7010b94e
SofiCadavid <scadavid.castro@udea.edu.co>
2026-04-25 19:21:21 -0500

    Creación de la clase manejadora de mat

--- Clases.py
+++ Clases.py
@@ -18,3 +18,25 @@ class CalAir:
         'ozono': {'nombre_visualizacion': 'Ozono', 'unidad': '(µg/m³)'},
         'co': {'nombre_visualizacion': 'Monóxido de Carbono', 'unidad': '(ppm)'}
     }
+
+class ManejadorMat:
+    """Clase para manipular de forma modular e independiente archivos .mat"""
+    
+    def __init__(self, ruta_archivo):
+        # Guarda la ruta del archivo seleccionado para usarla en los métodos
+        self.ruta = ruta_archivo
+        self.datos = None
+        self.info = None
+
+    def cargar_archivo(self):
+        """Carga el diccionario de datos del archivo .mat"""
+        try:
+            # sio.loadmat convierte el archivo .mat en un diccionario de Python
+            self.datos = sio.loadmat(self.ruta)
+            # sio.whosmat extrae los metadatos (nombres, dimensiones y tipo) sin cargar todo el peso
+            self.info = sio.whosmat(self.ruta)
+            return True
+        except Exception as e:
+            # En caso de error (archivo corrupto o no encontrado), avisa al usuario
+            print(f"Error al cargar: {e}")
+            return False
